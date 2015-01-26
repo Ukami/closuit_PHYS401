0c52ad02549a45fc06691e3e62456bbcb20a37b7
u421006155 <u421006155@thelxinoe.physics.tamu.edu>
2015-01-26 16:12:15 -0600

    second commit

---
+++ HW1/Q2/sort.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "math.h"
+#include "algorithm"
+
+int main (void)
+{
+	FILE unsorterd;
+	int N I=1;
+	scanf(%d,N);
+	printf("number of random elements=\n");
+	int sorted[N] unsorted[N];
+		while(fscanf(unsorted,"%d",unsorted[I])==1)
+	{
+		fscanf(unsorted,"%d",unsorted[I])=unsorted[I];
+		printf("%d",unsorted[4];
+	}	 
+	return 0;
+}
