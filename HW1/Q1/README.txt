a543680dd14c702186fdead91125c2c9871f995d
u421006155 <u421006155@thelxinoe.physics.tamu.edu>
2015-01-26 14:11:50 -0600

    first commit

---
+++ HW1/Q1/README.txt
@@ -0,0 +1,7 @@
+Prints 10 digits of Pi
+
+to compile type gcc Pi.c -lm 
+
+to excute ./a.out
+
+
---
+++ HW1/Q1/pi.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "math.h"
+
+int main(void)
+{
+   	printf("Pi to 10 digits=");
+	printf("3.1415926536\n");
+   	return 0;
+}
